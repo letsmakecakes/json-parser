b62a3985fd55702a5d863a2fb1f7be11ee3b3279
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-01-30 18:40:26 +0530

    Added constants

---
+++ pkg/lexer/lexer.go
@@ -0,0 +1,11 @@
+package lexer
+
+const (
+	JSONCOMMA        = ','
+	JSONCOLON        = ':'
+	JSONLEFTBRACKET  = '['
+	JSONRIGHTBRACKET = ']'
+	JSONLEFTBRACE    = '{'
+	JSONRIGHTBRACE   = '}'
+	JSONQUOTE        = '"'
+)

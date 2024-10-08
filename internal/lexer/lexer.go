37d8a331d9be0e7d5725f6e4c8a2949e3cafcabf
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-08 21:16:44 +0530

    added method to create lexer object and read character

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -26,3 +26,19 @@ type Lexer struct {
 	readPosition int  // current reading position in input (after current char)
 	ch           byte // current char under examination
 }
+
+func NewLexer(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}

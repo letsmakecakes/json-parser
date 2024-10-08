7d0bfc9f1ae1e10641501187b14510795f47e2bc
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-08 08:07:25 +0530

    added main program

--- cmd/jsonparser/main.go
+++ cmd/jsonparser/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	filepath := flag.String("file", "", "Path to the JSON fike to parse")
+	flag.Parse()
+
+	if *filepath == "" {
+		fmt.Println("Please provide a JSON file using -file flag.")
+		os.Exit(1)
+	}
+
+	data, err := ioutil.ReadFile(*filepath)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		os.Exit(1)
+	}
+
+	// TODO: Call lexer
+
+	// TODO: call Parser
+}

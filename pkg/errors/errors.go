f754df8160bea7cab3359df9c8b5b04f3b84190f
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-08 21:39:09 +0530

    added method to return new unexpected character error

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -1,1 +1,15 @@
 package errors
+
+import "fmt"
+
+type ParseError struct {
+	Message string
+}
+
+func (e *ParseError) Error() string {
+	return e.Message
+}
+
+func NewUnexpectedCharacterError(ch byte) error {
+	return &ParseError{Message: fmt.Sprintf("Unexpected charatcer: %q", ch)}
+}

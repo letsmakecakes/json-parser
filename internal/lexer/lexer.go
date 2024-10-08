ca24b5130e4d4a83240ee7814998a903f378594e
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-08 08:13:49 +0530

    added lexer struct model

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -19,3 +19,10 @@ type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+type Lexer struct {
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
+}

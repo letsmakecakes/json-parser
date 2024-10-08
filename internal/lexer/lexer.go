c9861dafcac00f40b82d758102288aff6d7a767d
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-08 08:11:02 +0530

    added token types

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -1,1 +1,16 @@
 package lexer
+
+type TokenType string
+
+const (
+	TokenLeftBrace  TokenType = "{"
+	TokenRightBrace TokenType = "}"
+	TokenColon      TokenType = ":"
+	TokenComma      TokenType = ","
+	TokenString     TokenType = "STRING"
+	TokenNumber     TokenType = "NUMBER"
+	TokenTrue       TokenType = "TRUE"
+	TokenFalse      TokenType = "FALSE"
+	TokenNull       TokenType = "NULL"
+	TokenEOF        TokenType = "EOF"
+)
